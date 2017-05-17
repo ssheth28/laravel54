622cca53120695363821f58f0c003e388a29454a
Sunshine <sunshine.mahant@gmail.com>
2017-05-17 17:05:06 +0000

    Apply fixes from StyleCI

--- app/Events/ChangePassword.php
+++ app/Events/ChangePassword.php
@@ -3,18 +3,17 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class ChangePassword
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $user_data;
+
     /**
      * Create a new event instance.
      *

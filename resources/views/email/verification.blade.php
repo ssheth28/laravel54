40899e10fa42f2caceaa7a2a99fa101fe53cc099
Sunny Sheth <ssheth@aecordigital.com>
2017-07-22 11:45:58 +0530

    email template related changes

--- resources/views/email/verification.blade.php
+++ resources/views/email/verification.blade.php
@@ -1,2 +1,2 @@
 <h1>Click the Link To Verify Your Email</h1>
-Click the following link to verify your email <a href="{{url('/verifyemail/'.$verification_token)}}">{{url('/verifyemail/'.$verification_token)}}</a>
+Click the following link to verify your email <a href="{{ route('verify.email', ['domain' => 'www', 'token' => $verification_token]) }}">{{route('verify.email', ['domain' => 'www', 'token' => $verification_token])}}</a>
